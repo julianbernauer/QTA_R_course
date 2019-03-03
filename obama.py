230254f691c905ab5c816b3ba9577137a0f0fe8e
Julian Bernauer <julian.bernauer@googlemail.com>
2019-03-03 12:21:16 +0100

    Add files via upload

---
+++ obama.py
@@ -0,0 +1,3 @@
+from nltk.tokenize import word_tokenize
+obama_tweet = "Zion Williamson seems like an outstanding young man as well as an outstanding basketball player. Wishing him a speedy recovery. https://twitter.com/barackobama."
+obama_source = word_tokenize(obama_tweet)
