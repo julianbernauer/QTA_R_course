80fe35b4c867b73d7901a42c4a9ce95951b806ed
Julian Bernauer <julian.bernauer@googlemail.com>
2019-03-18 16:56:45 +0100

    Create text_cleaning_exercise.R

---
+++ text_cleaning_exercise.R
@@ -0,0 +1,14 @@
+## QTA Course 18 March 2019 
+## Exercise text pre-processing and cleaning 
+
+# 1) Extract and clean all text snippets from Travis Bickle, the main character (played by Robert De Niro) 
+# using regex or other means 
+# 2) use the preText package to establish the variance of pre-processing specifications 
+# 3) report on your analysis 
+
+## "Taxi Driver" movie script 
+## source: https://www.imsdb.com/scripts/Taxi-Driver.html
+## hints obtaining the text: 
+# - copying returns some formatting -> might even be helpful 
+# - you could also try read_html() from the rvest package or a similar approach  
+# -> see https://www.r-bloggers.com/mining-game-of-thrones-scripts-with-r/
