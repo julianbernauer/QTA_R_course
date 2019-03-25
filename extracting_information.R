273b66b74525addbfcc4bedbba1eedc8d8ea7807
Julian Bernauer <julian.bernauer@googlemail.com>
2019-03-25 14:14:24 +0100

    Update extracting_information.R

--- extracting_information.R
+++ extracting_information.R
@@ -4,26 +4,27 @@ library(tidyverse)
 library(readtext)
 library(quanteda)
 
-# 1) Dictionaries 
-# Improve the dictionary and compare the results
+### 
+## Improve the populism dictionary and compare the results
 
 ## btw17 corpus 
 df_btw17 <- readtext("U:/QTA Kurs CDSS/data/btw2017", encoding="UTF-8-BOM")
 corpus_btw17 <- corpus(df_btw17)
-docvars(corpus_btw17, "party") <- c("Grüne","Linke","SPD","FDP","CDU","CSU","AfD")
+docvars(corpus_btw17, "party") <- c("GrÃ¼ne","Linke","SPD","FDP","CDU","CSU","AfD")
 
 # rooduijn and pauwels 
 pop_dict_deu <- dictionary(list(
   populism = c("elit*","konsens*","undemokratisch*","referend*","korrupt*",
-             "propagand*","politiker*","täusch*","betrüg*","betrug*",
-             "*verrat*","scham*","schäm*","skandal*","*wahrheit*",
-             "unfair*","unehrlich*","establishm*","*herrsch*","lüge*") 
+             "propagand*","politiker*","tÃ¤usch*","betrÃ¼g*","betrug*",
+             "*verrat*","scham*","schÃ¤m*","skandal*","*wahrheit*",
+             "unfair*","unehrlich*","establishm*","*herrsch*","lÃ¼ge*") 
 )) 
 
 
+
 ####
-# Sentiment of Lion King 
-# Get the [Lion King movie (1994) text](https://www.springfieldspringfield.co.uk/movie_script.php?movie=lion-king-the) and compare the sentiment to Taxi Driver 
+## Sentiment of Lion King 
+## Get the [Lion King movie (1994) text](https://www.springfieldspringfield.co.uk/movie_script.php?movie=lion-king-the) and compare the sentiment to Taxi Driver 
 
 # Taxi Driver from disk 
 df_taxi <- readtext("U:/QTA Kurs CDSS/data/taxidriver.txt", encoding="UTF-8")
@@ -33,9 +34,8 @@ taxi_sent
 
 
 
-
-### Exercise readability and lexical diversity 
-
+### 
+## Exercise readability and lexical diversity 
 ## EU Speeches (Verena) -> data provided 
 # Random sample of English texts: 
 # corpus_eu <- corpus(speeches_ep6_cleaned, text_field = "textEN")
